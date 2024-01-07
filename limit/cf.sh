e9e6eb07de3d65b74deb9c88215aaf5a6f9ec4a1
Silvergdr <155813425+Silvergdr@users.noreply.github.com>
2024-01-07 22:43:32 +0700

    Add files via upload

--- limit/cf.sh
+++ limit/cf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
-read -p "Masukan Domain (contoh : babi)" domen
+read -p "Masukan Domain (contoh : memei7)" domen
 DOMAIN=awibu.my.id
 sub=${domen}
 #sub=$(</dev/urandom tr -dc a-z | head -c4)
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
 dns=${sub}.awibu.my.id
-CF_ID=silvercyber1996@gmail.com
+CF_ID=Silvercyber1996@gmail.com
 CF_KEY=8cbad7474e818c740eccc1666bc83e52755e6
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
