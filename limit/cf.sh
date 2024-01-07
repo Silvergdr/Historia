14ee7e8d9ff226943b5024d3960e0f4d183d27e6
Silvergdr <155813425+Silvergdr@users.noreply.github.com>
2024-01-07 10:10:51 +0700

    Update cf.sh

--- limit/cf.sh
+++ limit/cf.sh
@@ -7,8 +7,8 @@ sub=${domen}
 #sub=$(</dev/urandom tr -dc a-z | head -c4)
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
 dns=${sub}.awibu.my.id
-CF_ID=bbcb2d68da5a2e03b9b3618eaf6de75f
-CF_KEY=7d7035383c7ecf43119060869f66684d
+CF_ID=silvercyber1996@gmail.com
+CF_KEY=8cbad7474e818c740eccc1666bc83e52755e6
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
